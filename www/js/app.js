69e0e742194124a13723e0fd59c896d9120161ff
higor <higor@butthead.viaconnect.com.br>
2015-06-17 00:37:43 -0300

    sinc

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,16 @@
 /**
+ *
  * Created by higor on 16/06/15.
  */
+var desafioApp = angular.module('desafioApp', [
+    'onsen',
+    'desafioControllers'
+]).config(['$httpProvider', '$interpolateProvider', function($httpProvider, $interpolateProvider) {
+    $interpolateProvider.startSymbol('{$');
+    $interpolateProvider.endSymbol('$}');
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+}]);
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,3 +1,14 @@
 /**
+ *
  * Created by higor on 16/06/15.
  */
+
+var desafioControllers = angular.module('desafioControllers', []);
+
+desafioControllers.controller('AppCtrl', ['$scope', function($scope) {
+    $scope.onClick = function(obj)
+    {
+        alert('click haha');
+    }
+}])
+
