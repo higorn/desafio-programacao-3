46fecf3f18a280a6c428e44b37bcb60056acb61d
Higor W. Nunes <higorn@gmail.com>
2016-03-23 15:45:36 -0300

    code climate

--- platforms/browser/www/js/controllers.js
+++ platforms/browser/www/js/controllers.js
@@ -32,14 +32,6 @@ desafioControllers.controller('AppCtrl', ['$scope', function($scope) {
                 if (!ret)
                     retSv = ret;
             }
-            /*
-            if ((!fld.val || fld.val == '') && key != 'foto')
-            {
-                $('#'+key).addClass('error-msg');
-                $scope.user[key] = {'error': 'O campo '+key+' é requerido'};
-                ret = false;
-            }
-            */
         });
         return retSv;
     }
@@ -53,6 +45,10 @@ desafioControllers.controller('AppCtrl', ['$scope', function($scope) {
             $('#'+id).addClass('error-msg');
             console.log('id: '+id);
             $scope.user[id] = {'error': obj.validationMessage};
+            if (obj.validity.typeMismatch && id == 'email')
+                $scope.user[id] = {'error': 'E-mail inválido'};
+            if (obj.validity.patternMismatch && id == 'cpf')
+                $scope.user[id] = {'error': 'Mínimo 11 dígitos'};
             ret = false;
         }
         else
@@ -64,45 +60,10 @@ desafioControllers.controller('AppCtrl', ['$scope', function($scope) {
         return ret;
     }
 
-    $scope.validate = function(obj, event)
+    $scope.validate = function(event)
     {
-        var fieldName = event.target.attributes.name.value;
         var id = event.target.attributes.id.value;
         isValid(id);
-
-/*
-        var inpObj = document.getElementById(id);
-        console.log('obj: '+fieldName);
-        if (inpObj.checkValidity() == false)
-        {
-            $scope.user[fieldName] = {'error': inpObj.validationMessage};
-        }
-        else
-        {
-            $('#'+fieldName).removeClass('error-msg');
-            delete $scope.user[fieldName]['error'];
-        }
-
-        if (obj.$dirty && obj.$invalid)
-        {
-            console.log('invalid: '+fieldName);
-            //$scope.user[fieldName] = {'error': 'Campo obrigatório'};
-            if (obj.$error.required)
-                $scope.user[fieldName] = {'error': 'Campo obrigatório'};
-            else if (obj.$error.email)
-                $scope.user[fieldName] = {'error': 'E-mail inválido'};
-            else if (obj.$error.pattern && fieldName == 'cpf')
-                $scope.user[fieldName] = {'error': 'CPF deve conter 11 digitos'};
-            else
-                $scope.user[fieldName] = {'error': obj.$error};
-        }
-        else
-        {
-            $('#'+fieldName).removeClass('error-msg');
-            delete $scope.user[fieldName]['error'];
-        }
-*/
-
     }
 
     $scope.add = function()
@@ -149,14 +110,14 @@ desafioControllers.controller('AppCtrl', ['$scope', function($scope) {
     $scope.takePic = function()
     {
         navigator.camera.getPicture(onSuccess, onFail, {
-            quality: 25,
+            quality: 10,
+            allowEdit: true,
             destinationType: Camera.DestinationType.DATA_URL
         });
     }
 
     var onSuccess = function(imageData)
     {
-        //$scope.user.foto.val = "data:image/jpeg;base64,"+imageData;
         $scope.user.foto.val = "data:image/jpeg;base64,"+imageData;
     }
 
